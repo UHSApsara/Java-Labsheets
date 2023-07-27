155f11a6b59cd0e3f9468e97f20d67de0cb6f566
UHSApsara <136603460+UHSApsara@users.noreply.github.com>
2023-07-27 21:18:51 +0530

    Add files via upload

---
+++ Lab 2/Part 01/Item1Obj.java
@@ -0,0 +1,8 @@
+package com.mycompany.item1.obj;
+public class Item1Obj {
+
+    public static void main(String[] args) {
+     Monster m1=new Monster(123,"Tangalle");
+     m1.display();
+    }
+}
---
+++ Lab 2/Part 01/Monster.java
@@ -0,0 +1,9 @@
+package com.mycompany.item1.obj;
+public class Monster extends Item {
+     public Monster(int l,String d){
+        super(l,d);
+    }
+   
+    
+}
+
